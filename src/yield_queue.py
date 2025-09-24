ecd9e74fb2cffc80e13532060843ba7357567d7d
Spraxxx <mrjacquesperiard@gmail.com>
2025-09-24 03:48:14 -0400

    Add YieldQueue class for managing kitchen outputs
    Implement YieldQueue class to manage kitchen outputs.

--- src/yield_queue.py
+++ src/yield_queue.py
@@ -1,1 +1,18 @@
+"""
+SPRAXXX Pantry – Yield Queue Module
+Purpose: Store outputs from Kitchen for nonprofit consumption
+Ethical: Nonprofit-only
+Branding: Always use SPRAXXX (S-P-R-A-X-X-X)
+"""
 
+class YieldQueue:
+    def __init__(self):
+        self.queue = []
+
+    def add_yield(self, yield_output):
+        """Add a Kitchen output to the queue."""
+        self.queue.append(yield_output)
+
+    def get_yield(self):
+        """Retrieve all queued outputs for nonprofit consumption."""
+        return self.queue
